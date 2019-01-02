44aeb9cd1c84185474805cbd8fec52761748191b
Momo Khan <snillmomo@gmail.com>
2019-01-02 16:21:58 -0500

    Added bootstrap to the project. Time to style the welcome page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_02_200733) do
+ActiveRecord::Schema.define(version: 2019_01_02_202532) do
+
+  create_table "cohorts", force: :cascade do |t|
+    t.string "name"
+    t.date "start_date"
+    t.date "end_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "courses", force: :cascade do |t|
     t.string "name"
