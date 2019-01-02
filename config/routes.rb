f28a37dc3f17393c1f87346326823c8a6cb49983
Momo Khan <snillmomo@gmail.com>
2019-01-02 18:02:52 -0500

    styled welcome page. Add better multiple selectors and then style the all users page

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :test_scaffolds
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
-  root 'pages#index'
+  root 'users#new'
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
