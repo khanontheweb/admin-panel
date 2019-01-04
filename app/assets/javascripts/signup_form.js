01717b2e9ec8bd02062ce655504281c137fd8b99
Momo Khan <snillmomo@gmail.com>
2019-01-04 14:02:22 -0500

    Made it so if the user already had a salary before when editing then it will be remembered if maybe the user editing accidentally switches to the wrong user type

--- app/assets/javascripts/signup_form.js
+++ app/assets/javascripts/signup_form.js
@@ -1,8 +1,14 @@
 var user_permission = document.getElementById("user_permission_id");
 console.log(user_permission.selectedIndex)
+var salary_id = document.getElementById("user_salary");
+var salary = salary_id.value;
 user_permission.addEventListener("change", function() {
-  if(user_permission.selectedIndex === 1)
+  if(user_permission.selectedIndex === 2) {
     document.getElementById("salary_column").classList.toggle("d-none");
-  else if(user_permission.selectedIndex !== 1)
+    salary_id.value = salary;
+  }
+  else if(user_permission.selectedIndex !== 2) {
     document.getElementById("salary_column").classList.toggle("d-none");
+    document.getElementById("user_salary").reset();
+  }
 });
